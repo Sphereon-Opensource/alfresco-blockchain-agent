41d89ce137d7886c47b6002812323cc3d172a633
LDMGN <lgijsen@sphereon.com>
2019-11-20 18:33:00 +0100

    Refactoring

--- blockchain-agent/src/main/java/com/sphereon/alfresco/blockchain/agent/rest/AlfrescoBlockchainController.java
+++ blockchain-agent/src/main/java/com/sphereon/alfresco/blockchain/agent/rest/AlfrescoBlockchainController.java
@@ -1,7 +1,6 @@
 package com.sphereon.alfresco.blockchain.agent.rest;
 
 import com.sphereon.alfresco.blockchain.agent.rest.model.ErrorResponse;
-import com.sphereon.alfresco.blockchain.agent.rest.model.VerifyContentAlfrescoResponse;
 import com.sphereon.alfresco.blockchain.agent.rest.model.VerifyNodesRequest;
 import com.sphereon.alfresco.blockchain.agent.rest.model.VerifyNodesResponse;
 import com.sphereon.alfresco.blockchain.agent.tasks.ondemand.VerifyRegistrations;
@@ -13,16 +12,13 @@ import io.swagger.annotations.ApiResponses;
 import org.slf4j.MDC;
 import org.slf4j.ext.XLogger;
 import org.slf4j.ext.XLoggerFactory;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.List;
 
 @RestController(value = "Alfresco blockchain functions")
 @Api(description = "API for blockchain functions using the Alfresco content repository", value = "API for blockchain functions", tags = Constants.Tags.BLOCKCHAIN)
@@ -43,27 +39,24 @@ public class AlfrescoBlockchainController {
             @ApiResponse(code = 400, message = "Verification request failed", response = ErrorResponse.class),
             @ApiResponse(code = 404, message = "One or more node id's could not be found", response = ErrorResponse.class)
     })
-    @ResponseStatus(code = HttpStatus.OK) // Needed to override the default 200 code
     @PostMapping(value = Constants.Endpoints.AlfrescoBlockchain.VERIFY_ENTRIES, consumes = {MediaType.APPLICATION_JSON_UTF8_VALUE}, produces = {MediaType.APPLICATION_JSON_UTF8_VALUE})
     public VerifyNodesResponse verifyEntries(@ApiParam(required = true, name = Constants.Param.NODE_IDS) @RequestBody final VerifyNodesRequest verifyNodesRequest,
                                              final HttpServletRequest request) {
         if (logger.isDebugEnabled()) {
             logger.entry(verifyNodesRequest);
         }
-        Assert.notNull(verifyNodesRequest, "verifyNodesRequest is null!");
-        Assert.notNull(verifyNodesRequest.getNodeIds(), "verifyNodesRequest.nodeIds is null!");
 
-        final List<VerifyContentAlfrescoResponse> result = verifyRegistrations.execute(verifyNodesRequest.getNodeIds(), credentials(request));
+        Assert.notNull(verifyNodesRequest, "Non-null verifyNodesRequest expected");
+        Assert.notNull(verifyNodesRequest.getNodeIds(), "Non-null verifyNodesRequest.nodeIds expected");
+
+        final var credentials = request.getHeader(HEADER_AUTHORIZATION);
+        final var verifyResponses = verifyRegistrations.execute(verifyNodesRequest.getNodeIds(), credentials);
         if (logger.isDebugEnabled()) {
-            logger.exit(result);
+            logger.exit(verifyResponses);
         }
         MDC.clear();
 
         // TODO: throw RestExceptions with documented http codes
-        return VerifyNodesResponse.wrap(result);
-    }
-
-    private String credentials(final HttpServletRequest request) {
-        return request.getHeader(HEADER_AUTHORIZATION);
+        return VerifyNodesResponse.wrap(verifyResponses);
     }
 }
